4124bdff5257a7f9b16d102fd4526b4c5473ee5e
algion <mralgion@gmail.com>
2019-08-18 11:45:22 +0300

    установка страницы пустого фильтра

--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from "react-redux";
 
 import {MovieList} from "../components";
 
-const MainPage = ({ movies }) => (
-        <div className="movieList-wrap">
-            {movies.map(item => <MovieList key={item._id} movie={item} /> )}
-        </div>
-    );
+const MainPage = ({ movies }) => {
+    const [filteredMovies, setFilteredMovies] = useState([]);
+    const [valueInput, setValueInput] = useState("")
+    const handleChangeInput = (e) => {
+        const { value } = e.target;
+        setValueInput(value);
+        setFilteredMovies(movies.filter(item => (
+            item.title.toLowerCase().includes(value.toLowerCase()))
+        ));
+    };
 
+    return (
+        <div className="movieList-wrap">
+            <input
+                type="text"
+                name="filter-name"
+                onChange={handleChangeInput}
+                value={valueInput}
+            />
+            {filteredMovies.map(item =>(
+                <MovieList key={item._id} movie={item} /> )
+            )}
+        </div>
+    )
+};
 
 const mapStateToProps = (state) => ({
    movies: state.data.movies
